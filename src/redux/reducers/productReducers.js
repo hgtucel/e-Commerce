955e9deffb94b71568a1d629c7029b0c0bc52620
Hakkı Gürkan Tüçel <ben@hgtucel.com>
2020-08-08 15:14:12 +0300

    Create, Update, Delete Request added.

--- src/redux/reducers/productReducers.js
+++ src/redux/reducers/productReducers.js
@@ -3,7 +3,7 @@ import * as actionTypes from '../actions/actionTypes';
 function productListReducer(state={products:[]}, action) {
     switch (action.type) {
         case actionTypes.PRODUCT_LIST_REQUEST:
-            return {loading: true}
+            return {loading: true, products:[]}
         case actionTypes.PRODUCT_LIST_SUCCESS:
             return {loading: false, products: action.payload}
         case actionTypes.PRODUCT_LIST_FAIL:
@@ -26,4 +26,30 @@ function productDetailsReducer(state={product:{}}, action) {
     }
 }
 
-export {productListReducer, productDetailsReducer}
+function saveProductReducer(state={product:{}}, action) {
+    switch (action.type) {
+        case actionTypes.SAVE_PRODUCT_REQUEST:
+            return {loading: true}
+        case actionTypes.SAVE_PRODUCT_SUCCESS:
+            return {loading: false, success: true, product: action.payload}
+        case actionTypes.SAVE_PRODUCT_FAIL:
+            return {loading: false, error: action.payload}
+        default:
+            return state;
+    }
+}
+
+function deleteProductReducer(state={product:{}}, action) {
+    switch (action.type) {
+        case actionTypes.DELETE_PRODUCT_REQUEST:
+            return {loading: true}
+        case actionTypes.DELETE_PRODUCT_SUCCESS:
+            return {loading: false, success: true, product: action.payload}
+        case actionTypes.DELETE_PRODUCT_FAIL:
+            return {loading: false, error: action.payload}
+        default:
+            return state;
+    }
+}
+
+export {productListReducer, productDetailsReducer, saveProductReducer, deleteProductReducer}
